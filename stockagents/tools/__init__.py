dc52fcddfef21b558a309cdc4804365e74b32442
Uri Levy <8178470+uriktm@users.noreply.github.com>
2025-10-20 16:29:34 +0300

    Add SocialSentimentTool for Reddit and X analysis

--- stockagents/tools/__init__.py
+++ stockagents/tools/__init__.py
@@ -2,10 +2,12 @@
 
 from .corporate_events import CorporateEventsTool
 from .news_and_buzz import NewsAndBuzzTool
+from .social_sentiment import SocialSentimentTool
 from .volume_and_technicals import VolumeAndTechnicalsTool
 
 __all__ = [
     "CorporateEventsTool",
     "NewsAndBuzzTool",
+    "SocialSentimentTool",
     "VolumeAndTechnicalsTool",
 ]
